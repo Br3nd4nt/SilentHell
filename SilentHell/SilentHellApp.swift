541b5981a17765b7cf2e429f2113efc5a3bc563b
Br3nd4nt <Vlad.belov.08@gmail.com>
2026-06-22 22:12:03 +0300

    Initial Commit

---
+++ SilentHell/SilentHellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentHellApp.swift
+//  SilentHell
+//
+//  Created by br3nd4nt on 22.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct SilentHellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
